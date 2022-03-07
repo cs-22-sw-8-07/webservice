bf25dc27ef169983434181dc0d683712ed848d29
japr <japr@tarp.dk>
2022-03-07 12:03:10 +0100

    Added things

--- quack_api/quack_api.Test/IntegrationTests/IntegrationTests.cs
+++ quack_api/quack_api.Test/IntegrationTests/IntegrationTests.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using quack_api;
+using System.Net.Http;
 
 namespace quack_api.Test.IntegrationTests
 {
+    [TestClass]
     public class IntegrationTests
     {
         /// <summary>
@@ -21,7 +24,7 @@ namespace quack_api.Test.IntegrationTests
             var configurationBuilder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
-
+            
             var webHostBuilder =
                   new WebHostBuilder()
                   .UseConfiguration(configurationBuilder)
@@ -30,5 +33,25 @@ namespace quack_api.Test.IntegrationTests
             // Return test server
             return new TestServer(webHostBuilder);
         }
+
+        [TestMethod]
+        public async Task IssueController_GetIssueDetails_CorrectJSONResponse()
+        {
+            // Arrange
+            using (var server = GetTestServer())
+            using (var client = server.CreateClient())
+            {
+                // Act
+                HttpResponseMessage responseMessage = await client.GetAsync("/WASP/Issue/GetIssueDetails?issueId=1");
+                string content = await responseMessage.Content.ReadAsStringAsync();
+
+                // Assert
+                Assert.AreEqual(System.Net.HttpStatusCode.OK, responseMessage.StatusCode);
+                Assert.AreEqual("{\"Result\":{\"Id\":1,\"CitizenId\":1,\"Description\":\"Der er en løs flise\",\"DateCreated\":\"2021-10-21T13:44:15\",\"Location\":{\"Latitude\":57.012218,\"Longitude\":9.99433},\"Address\":\"Alfred Nobels Vej 27, 9200 Aalborg, Danmark\",\"Category\":{\"Id\":3,\"Name\":\"Fortov\"},\"SubCategory\":{\"Id\":11,\"CategoryId\":3,\"Name\":\"Løse fliser\"},\"Municipality\":{\"Id\":1,\"Name\":\"Aalborg\"},\"IssueState\":{\"Id\":1,\"Name\":\"Oprettet\"},\"MunicipalityResponses\":[],\"IssueVerificationCitizenIds\":[2,7]},\"IsSuccessful\":true,\"ErrorNo\":0}",
+                                 content);
+            }
+        }
+
+
     }
 }
--- quack_api/quack_api.Test/UnitTests/ControllerUnitTests.cs
+++ quack_api/quack_api.Test/UnitTests/ControllerUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace quack_api.Test.UnitTests
 {
-    class ControllerUnitTests
+    [TestClass]
+    public class ControllerUnitTests
     {
+        /*[TestMethod]
+        [TestCategory(nameof(CitizenController.GetListOfCitizens))]
+        public async Task IssueController_GetListOfCitizens_Success()
+        {
+            // Arrange
+            int municipalityId = 1;
+            var contextFactory = new MockHiveContextFactory();
+            CitizenController controller = new(contextFactory);
+
+            // Act            
+            var result = await controller.GetListOfCitizens(municipalityId, true);
+
+            // Assert                
+            using (var context = contextFactory.CreateDbContext())
+            {
+                Assert.IsTrue(result.Value.IsSuccessful);
+                Assert.IsInstanceOfType(result.Value.Result, typeof(IEnumerable<CitizenDTO>));
+            }
+        }*/
     }
 }
