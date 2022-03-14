5008f08289573f4228b95714383b884ecfe7f10c
Jeppe Lindberg <jepp3441@gmail.com>
2022-03-14 13:47:10 +0100

    fix gitignore

--- quack_api/quack_api/Program.cs
+++ quack_api/quack_api/Program.cs
@@ -13,7 +13,8 @@ namespace quack_api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("test");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
