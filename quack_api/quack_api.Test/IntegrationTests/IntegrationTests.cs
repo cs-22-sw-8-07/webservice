3c47e2db0c7522121820b77203d33df7710b9734
Liv Holm <43985985+LivHolm@users.noreply.github.com>
2022-04-06 15:04:54 +0200

    Made an intergration test using a TestServer
    The test calls the getPlaylist action, without parameters, and this seems to function

--- quack_api/quack_api.Test/IntegrationTests/IntegrationTests.cs
+++ quack_api/quack_api.Test/IntegrationTests/IntegrationTests.cs
@@ -9,25 +9,27 @@ using System.Text;
 using System.Threading.Tasks;
 using quack_api;
 using System.Net.Http;
+using quack_api.Models;
 
 namespace quack_api.Test.IntegrationTests
 {
     [TestClass]
     public class IntegrationTests
     {
+        public IConfiguration Configuration { get; set; }
         /// <summary>
         /// Sets up a local test server where the configuration is giving through the appsettings file.
         /// </summary>
         /// <returns>a test server</returns>
         private TestServer GetTestServer()
         {
-            var configurationBuilder = new ConfigurationBuilder()
+            Configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
             
             var webHostBuilder =
                   new WebHostBuilder()
-                  .UseConfiguration(configurationBuilder)
+                  .UseConfiguration(Configuration)
                         .UseEnvironment("production") // You can set the environment you want (development, staging, production)
                         .UseStartup<Startup>(); // Startup class of your web app project
             // Return test server
@@ -42,13 +44,12 @@ namespace quack_api.Test.IntegrationTests
             using (var client = server.CreateClient())
             {
                 // Act
-                HttpResponseMessage responseMessage = await client.GetAsync("/Quack/Issue/GetIssueDetails?issueId=1");
+                HttpResponseMessage responseMessage = await client.GetAsync("/Quack/Recommender/GetPlaylist");
                 string content = await responseMessage.Content.ReadAsStringAsync();
 
                 // Assert
                 Assert.AreEqual(System.Net.HttpStatusCode.OK, responseMessage.StatusCode);
-                Assert.AreEqual("{\"Result\":{\"Id\":1,\"CitizenId\":1,\"Description\":\"Der er en løs flise\",\"DateCreated\":\"2021-10-21T13:44:15\",\"Location\":{\"Latitude\":57.012218,\"Longitude\":9.99433},\"Address\":\"Alfred Nobels Vej 27, 9200 Aalborg, Danmark\",\"Category\":{\"Id\":3,\"Name\":\"Fortov\"},\"SubCategory\":{\"Id\":11,\"CategoryId\":3,\"Name\":\"Løse fliser\"},\"Municipality\":{\"Id\":1,\"Name\":\"Aalborg\"},\"IssueState\":{\"Id\":1,\"Name\":\"Oprettet\"},\"MunicipalityResponses\":[],\"IssueVerificationCitizenIds\":[2,7]},\"IsSuccessful\":true,\"ErrorNo\":0}",
-                                 content);
+                Assert.IsTrue(content.StartsWith("{\"is_successful\":true,\"error_no\":0}"));
             }
         }
 
